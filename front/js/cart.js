76be13fd1d059418bb41472841eec939bc8f8b85
frederick486 <frederick.gaittet@orange.fr>
2022-08-02 15:18:58 +0200

    Affichage des articles dans la page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,63 +1,69 @@
 
-let items = JSON.parse(localStorage.getItem("basket"))
-
-let id = items[0][0]
-let color = items[0][1]
-let qty = items[0][2]
-
-///////////////////////////////////////////////////////////
-
-let article
+let basket = JSON.parse(localStorage.getItem("basket"))
 
 
-(async function() {
-    const article = await getArticle(id)
-    console.log(article)
-    displayArticle(article)
-})()
+for (let i = 0; i < basket.length; i++) {
+     
+    let id = basket[i][0]
+    let color = basket[i][1]
+    let qty = basket[i][2]
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+    // ???????? article est déclaré mais sa valeur n'est jamais lue, 
+    // pourtant quand on la supprime, l'affichage ne fonctionne plus ...
+    let article
 
 
-//Récupération des infots de l'article depuis l'API
-function getArticle() {
-    return fetch(`http://localhost:3000/api/products/${id}`)      
-        .then(function(httpBodyResponse) {                  
-            return httpBodyResponse.json()                 
-        })        
-        .then(function(article) {                     
-            return article                     
-        })
-        .catch(function(error) {                            
-            alert(error)
-        })
-}
+    (async function() {
+        const article = await getArticle(id)
+        // console.log(article)
+        displayArticle(article)
+    })()
 
-///////////////////////////////////////////////////
 
-//AFFICHAGE
-function displayArticle(article) {
-    document.getElementById("cart__items").innerHTML += `
+    //Récupération des infots de l'article depuis l'API
+    function getArticle() {
+        return fetch(`http://localhost:3000/api/products/${id}`)      
+            .then(function(httpBodyResponse) {                  
+                return httpBodyResponse.json()                 
+            })        
+            .then(function(article) {                     
+                return article                     
+            })
+            .catch(function(error) {                            
+                alert(error)
+            })
+    }
 
-    <article class="cart__item" data-id="${id}" data-color="${color}">
-        <div class="cart__item__img">
-        <img src="${article.imageUrl}" alt="${article.altTxt}">
-        </div>
-        <div class="cart__item__content">
-        <div class="cart__item__content__description">
-            <h2>${article.name}</h2>
-            <p>${color}</p>
-            <p>${article.price} €</p>
-        </div>
-        <div class="cart__item__content__settings">
-            <div class="cart__item__content__settings__quantity">
-            <p>Qté : </p>
-            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${qty}">
+/////////////////////////////////////////////////////////////////////////////////////
+
+    //AFFICHAGE
+    function displayArticle(article) {
+        document.getElementById("cart__items").innerHTML += `
+
+        <article class="cart__item" data-id="${id}" data-color="${color}">
+            <div class="cart__item__img">
+            <img src="${article.imageUrl}" alt="${article.altTxt}">
             </div>
-            <div class="cart__item__content__settings__delete">
-            <p class="deleteItem">Supprimer</p>
+            <div class="cart__item__content">
+            <div class="cart__item__content__description">
+                <h2>${article.name}</h2>
+                <p>${color}</p>
+                <p>${article.price} €</p>
             </div>
-        </div>
-        </div>
-    </article> 
-    
-    `    
+            <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                <p>Qté : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${qty}">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
+                </div>
+            </div>
+            </div>
+        </article> 
+        
+        `    
+    }
 }
